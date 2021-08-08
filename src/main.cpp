2bfab76f82b27ea16e8754dd33ca38fe0582c353
Elie Hatem <e.na.hatem@gmail.com>
2021-08-08 15:12:18 +0200

    Added the files

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+/* const int fib(int n){
+    if (n<=2)
+    return 1;
+
+    int x = 0;
+    int y = 0;
+    int z = 0;
+
+    x = fib(n-2);
+    y = fib(n-1);
+    z = x + y;
+    
+    return z;
+}*/
+
+int main(){
+
+   // const int ans = fib(7); 
+
+    std::cout<< "Hello world!" << std::endl;
+    // std::cout<< fib(3) << std::endl;
+    // std::cout<< fib(0) << std::endl;
+    
+    }
+
